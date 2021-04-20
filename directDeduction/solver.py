3e45a1afe18162fde11da36711a049d98434c780
vincentlie06 <72127176+vincentlie06@users.noreply.github.com>
2021-04-20 14:08:46 +0700

    more explicit import

--- directDeduction/solver.py
+++ directDeduction/solver.py
@@ -1,19 +1,20 @@
 #Solving a sudoku with only with direct deduction and elimination
-from boardTools import *
+#Full algorithm please see README.md file.
+import boardTools as tool
 
-def solve(matrix: Matrix, showStatus = True)-> Matrix:
-    emptyLocs = getLocState(matrix)
+def solve(matrix: tool.Matrix, showStatus = True)-> tool.Matrix:
+    emptyLocs = tool.getEmptyLocs(matrix)
     matrix_size = len(matrix)
     prev = len(emptyLocs)
     if showStatus: print(f'Empty location length in the beginning: {prev}')
     while emptyLocs:
-        boxes = makeBoxes(matrix)
-        possibleVals = {pos : getPossibleValue(pos, matrix, boxes) for pos in emptyLocs}
+        boxes = tool.makeBoxes(matrix)
+        possibleVals = {pos : tool.getPossibleValue(pos, matrix, boxes) for pos in emptyLocs}
         item = possibleVals.items()
         for a, b in item:
             if len(b) == 1:
                 matrix[a[0]][a[1]] = b[0]
-        emptyLocs = getLocState(matrix)
+        emptyLocs = tool.getEmptyLocs(matrix)
         if len(emptyLocs) == prev:
             print("This sudoku arrangement in unsolveable by direct deduction, this is the furthest i can do")
             return matrix
